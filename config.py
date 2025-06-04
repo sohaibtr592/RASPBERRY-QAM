4477be0a3e7605a66ce6ce326a37e4c0c9096a0f
sohaibtr592 <tardaoui.sohaib@etu.uae.ac.ma>
2025-06-04 12:25:58 +0100

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+RECEIVER_IP = "IP_ADRESS"  # Replace with the IP of the other Raspberry Pi
+PORT = 5005
+STARTER_BIT = np.array([1, 0, 1, 0, 1, 0, 1, 0], dtype=np.uint8)
+AUTH_PASSWORD = "raspberry_secure_chat"
+
+
+
+
